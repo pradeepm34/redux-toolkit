b5b770dafa060b1943d13e14150b8ac977dea235
pradeepm34 <pradeepmprs@gmail.com>
2023-01-30 20:00:18 -0600

    inital commit

---
+++ README_DEV.md
@@ -0,0 +1,4 @@
+diff between old and new redux(rtk)
+what is mutation in JS?
+can you mutate state in redux?
+diff between creating actions with redux and rtk . what immer is doing behind scenes?
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+};
+export default App;
